2272b0342c32ea4630cc0160df5bc43173a8e8e7
Sunny Kim <102301310+snusunny@users.noreply.github.com>
2022-05-08 21:12:02 +0900

    Feat: default singleton via SO (@tehokim)

--- Assets/Editor/EUtils.cs
+++ Assets/Editor/EUtils.cs
@@ -4,11 +4,33 @@ using UnityEditor;
 using UnityEngine;
 
 public static class EUtils {
+    private const string RES_DIR = "Assets/Resources";
+    private const string DEFAULTS_DIR = "Assets/Resources/DefaultValues";
+
     public static T FromPath<T>(string path) where T : ScriptableObject {
         return AssetDatabase.LoadAssetAtPath<T>(path);
     }
 
     public static void SetDefault<T>(T obj) where T : ScriptableObject {
         Debug.Log("TODO");
+
+        if (!AssetDatabase.IsValidFolder(RES_DIR)) {
+            AssetDatabase.CreateFolder("Assets", "Resources");
+        }
+        if (!AssetDatabase.IsValidFolder(DEFAULTS_DIR)) {
+            AssetDatabase.CreateFolder("Assets/Resources", "DefaultValues");
+        }
+
+        string path = DEFAULTS_DIR + "/Default" + typeof(T).Name + ".asset";
+
+        T defo = AssetDatabase.LoadAssetAtPath<T>(path);
+        if (defo == null) {
+            defo = ScriptableObject.CreateInstance<T>();
+            AssetDatabase.CreateAsset(defo, path);
+        }
+
+        EditorUtility.CopySerialized(obj, defo);
+        EditorUtility.SetDirty(defo);
+        Debug.Log("Set default of " + typeof(T).Name + " to " + obj.name);
     }
 }
